e6c46335f8e3b3a424e565b9602f54a7dec3c235
CheDil <168687052+CheDil@users.noreply.github.com>
2025-03-07 14:53:54 +0530

    Added the Load cell part
    Library and the code is added to the main.

--- Turbo_jet_electrical/src/main.cpp
+++ Turbo_jet_electrical/src/main.cpp
@@ -1,12 +1,27 @@
 #include <Arduino.h>
 #include<Servo.h>
+#include<HX711.h>
+
 
 Servo servo_motor;
+const int Loadcell_dout = 2;
+const int Loadcell_sck = 3;
 
+//calibration_readings 
+int NMSAMPLES;
+float calibration_factor = 7050.0;
+
+HX711 Loadcell(Loadcell_dout, Loadcell_sck);
 int servo_motor_mechanism(float);
+float Weight_reading();
+
 
 void setup() {
   servo_motor.attach(9);
+  Loadcell.begin(Loadcell_dout, Loadcell_sck);
+  Loadcell.set_scale(calibration_factor);
+  Loadcell.tare();
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -18,4 +33,9 @@ int servo_motor_mechanism(float angle) {
   servo_motor.write(angle);
   delay(1000);
   return 0;
+}
+
+float Weight_reading() {
+  float weight = Loadcell.get_units(NMSAMPLES);
+  return weight;
 }
